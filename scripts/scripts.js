d932ffc7894a08f9eca8dba9d3c808dd66bdfb1c
villiamrose <villiamrose@live.com>
2022-09-24 21:59:29 +0800

    Implement add, subtract, multiply, and divide functions

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+// returns sum of x and y
+function add(x, y) {
+  return x + y;
+}
+
+// returns difference of x and y
+function subtract(x, y) {
+  return x - y;
+}
+
+// returns product of x and y
+function multiply(x, y) {
+  return x * y;
+}
+
+// returns quotient of x and y
+function divide(x, y) {
+  return x / y;
+}
