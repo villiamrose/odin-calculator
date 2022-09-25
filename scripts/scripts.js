c9697be2e9639e85e05a2c3c4c50b9d13940a3e4
villiamrose <villiamrose@live.com>
2022-09-25 20:10:47 +0800

    Create Algebra class to handle mathematical methods

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,26 @@
-// returns sum of x and y
-function add(x, y) {
-  return x + y;
-}
+class Algebra {
+  // returns sum of x and y
+  static add(x, y) {
+    return x + y;
+  }
 
-// returns difference of x and y
-function subtract(x, y) {
-  return x - y;
-}
+  // returns difference of x and y
+  static subtract(x, y) {
+    return x - y;
+  }
 
-// returns product of x and y
-function multiply(x, y) {
-  return x * y;
-}
+  // returns product of x and y
+  static multiply(x, y) {
+    return x * y;
+  }
 
-// returns quotient of x and y
-function divide(x, y) {
-  return x / y;
-}
+  // returns quotient of x and y
+  static divide(x, y) {
+    return x / y;
+  }
 
-// calls func on x and y
-function operate(func, x, y) {
-  return func(x, y);
+  // calls func on x and y
+  static operate(func, x, y) {
+    return func(x, y);
+  }
 }
